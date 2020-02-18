51953ebd880fe49d7e570f28373b4c48a6a5e778
angump <angump@AGI-LAP-166.DCDMC.LOCAL>
2020-02-18 09:30:15 +0400

    Added mapper

---
+++ src/main/java/guru/springframework/msscbeerservice/web/mapper/BeerMapper.java
@@ -0,0 +1,17 @@
+package guru.springframework.msscbeerservice.web.mapper;
+
+import org.mapstruct.Mapper;
+
+import guru.springframework.msscbeerservice.web.domain.Beer;
+import guru.springframework.msscbeerservice.web.model.BeerDto;
+
+
+
+@Mapper(uses = { DateMapper.class })
+public interface BeerMapper {
+
+	BeerDto beerToBeerDto(Beer beer);
+
+	Beer beerDtoToBeer(BeerDto beerDto);
+
+}

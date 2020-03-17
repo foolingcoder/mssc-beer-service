d594c2918ce7068aade415eba9dfa1dbc44d04ed
angump <angump@host.docker.internal>
2020-03-17 11:37:04 +0400

    refactored

--- src/main/java/guru/springframework/model/BeerStyleEnum.java
+++
@@ -1,6 +0,0 @@
-package guru.springframework.model;
-
-public enum BeerStyleEnum {
-	LAGER, PILSNER, STOUT, GOSE, PORTER, ALE, WHEAT, IPA, PALE_ALE, SAISON
-
-}
--- src/main/java/guru/springframework/msscbeerservice/web/service/Inventory/BeerInventoryService.java
+++
@@ -1,9 +0,0 @@
-package guru.springframework.msscbeerservice.web.service.Inventory;
-
-import java.util.UUID;
-
-public interface BeerInventoryService {
-	
-	Integer getOnhandInventory(UUID beerId);
-
-}

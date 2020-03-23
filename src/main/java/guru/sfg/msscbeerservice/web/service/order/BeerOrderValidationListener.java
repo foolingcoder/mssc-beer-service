2ad088e9128b900eed2b49ee63eb7ced066dd15c
angump <angump@host.docker.internal>
2020-03-23 13:01:33 +0400

    Refactored code

--- src/main/java/guru/sfg/msscbeerservice/web/service/order/BeerOrderValidationListener.java
+++ src/main/java/guru/sfg/msscbeerservice/web/service/order/BeerOrderValidationListener.java
@@ -1,36 +1,28 @@
 package guru.sfg.msscbeerservice.web.service.order;
 
-import javax.transaction.Transactional;
-
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.jms.core.JmsTemplate;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Component;
 
 import guru.sfg.brewery.model.events.ValidateOrderRequest;
 import guru.sfg.brewery.model.events.ValidateOrderResult;
 import guru.sfg.msscbeerservice.config.JmsConfig;
 import lombok.RequiredArgsConstructor;
 
-
-@Service
 @RequiredArgsConstructor
+@Component
 public class BeerOrderValidationListener {
 
-    private final BeerOrderValidator validator;
+	private final BeerOrderValidator validator;
 
 	private final JmsTemplate jmsTemplate;
 
-	@Transactional
 	@JmsListener(destination = JmsConfig.VALIDATE_ORDER_QUEUE)
 	public void listen(ValidateOrderRequest validateOrderRequest) {
 		Boolean isValid = validator.validateOrder(validateOrderRequest.getBeerOrder());
 
-        jmsTemplate.convertAndSend(JmsConfig.VALIDATE_ORDER_RESPONSE_QUEUE,
-                ValidateOrderResult.builder()
-                    .isValid(isValid)
-                    .orderId(validateOrderRequest.getBeerOrder().getId())
-                    .build());
-
+		jmsTemplate.convertAndSend(JmsConfig.VALIDATE_ORDER_RESPONSE_QUEUE, ValidateOrderResult.builder()
+				.isValid(isValid).orderId(validateOrderRequest.getBeerOrder().getId()).build());
 
 	}
 
